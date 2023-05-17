7a5b6d3fcae6b98a35e8147e1fddf42fe8df6456
jdgrobeck <jdgrobeck@gmail.com>
2023-05-17 11:29:24 -0700

    updated bet data

--- src/components/Games.js
+++ src/components/Games.js
@@ -24,13 +24,22 @@ import { format, parseISO, isToday } from 'date-fns';
 
 
 const Games = () => {
+
+
 console.log(localStorage)
 const [nba, setNba] = useState([]);
 //This is where we store a selected game
 const [selectedGame, setSelectedGame] = useState(null);
 const [awaySpread, setAwaySpread] = useState(null); // Added state for spread
 const [homeSpread, setHomeSpread] = useState(null); // Added state for spread
+const [bets, setBets] = useState([])
 // const [odds, setOdds] = useState([]);
+
+const userId = localStorage.getItem("user id")
+
+let awayTeamBetData = null;
+let homeTeamBetData = null;
+
 
   useEffect(() => {
    axios.get('https://capstone-planning.vercel.app/games')
@@ -39,16 +48,23 @@ const [homeSpread, setHomeSpread] = useState(null); // Added state for spread
 
   }, []); // empty array ensures that the effect runs only once, when the component is mounted.
 
-  
+  useEffect(() => {
+   axios.get(`https://capstone-planning.vercel.app/bets/${userId}`)
+   .then(response => setBets(response.data))
 
-  let awayTeamBetData = null;
-  let homeTeamBetData = null;
-    
+
+  }, [userId]); // empty array ensures that the effect runs only once, when the component is mounted.
+
+  console.log(bets)
+
 
   const handleSelectedGame = (event) => {
     const gameId = event.target.value;
     const selectedGame = nba.find(game => game.id === gameId);
     setSelectedGame(selectedGame);
+    console.log(selectedGame)
+    console.log(selectedGame.away_team)
+    console.log(selectedGame.home_team)
     //How do I map over the selectedGame?
     //I think I can map here to get every spread. For now, just returning spread for away team.
     // This works for the away team. Now, how do I get the spread for the home team?
@@ -94,23 +110,36 @@ const [homeSpread, setHomeSpread] = useState(null); // Added state for spread
 // if home team is picked, send this betsData with pick: selectedGame.home_team. Else send pick: selectedGame.away_team. How do I differentiate the buttons?
 // I believe I could also define homeTeamBetData and awayTeamBetData outside of handleSelectedGame and pass them as parameters to handleWinnerPick
 
+const homeTeam = selectedGame.home_team
+const awayTeam = selectedGame.away_team
+
 const handleWinnerPick = (event) => {
   const betsUrl = "https://capstone-planning.vercel.app/bets"
+
+  // const homeTeam = selectedGame ? selectedGame.home_team : null;
+  // const awayTeam = selectedGame ? selectedGame.away_team : null;
+  
+  
   homeTeamBetData = selectedGame ? {
-    user_id: localStorage.getItem("user id"),
+    user_id: userId,
     game_id: selectedGame.id,
     commence_time: selectedGame.commence_time,
+    home_team: homeTeam,
+    away_team: awayTeam,
     // IT's sport_title in the API but sport in my db. Which one do I use?
-    sport: selectedGame.sport,
+    sport: selectedGame.sport_title,
     pick: selectedGame.home_team,
     spread: homeSpread // assuming homeSpread is already set by the previous logic
   } : null;
 
   awayTeamBetData = selectedGame ? {
-    user_id: localStorage.getItem("user id"),
+    user_id: userId,
     game_id: selectedGame.id,
     commence_time: selectedGame.commence_time,
-    sport: selectedGame.sport,
+    //Why isn't it capturing this?
+    // home_team: selectedGame.home_team,
+    // away_team: selectedGame.away_team,
+    sport: selectedGame.sport_title,
     pick: selectedGame.away_team,
     spread: awaySpread // assuming awaySpread is already set by the previous logic
   } : null;
@@ -118,9 +147,11 @@ const handleWinnerPick = (event) => {
   event.preventDefault()
 
   let buttonValue = event.target.value;
+  console.log(event.target.value)
 
   if (buttonValue === "away-team-pick"){
     axios.post(betsUrl, awayTeamBetData)
+
     .then(response => {
       console.log('Data sent successfully:', response.data);
       // Handle success, such as showing a success message, updating state, etc.
@@ -141,6 +172,8 @@ const handleWinnerPick = (event) => {
       // Handle error, such as showing an error message, etc.
     });
   }
+window.alert("Bet submitted. Good luck!")
+window.location.reload()
 }
 
 // filtering out based on the current date
@@ -176,8 +209,42 @@ const todaysGames = nba.filter(game => isToday(parseISO(game.commence_time)));
           {/* <p>Spread for {selectedGame.home_team}: {spread[1][2]}</p> */}
         </div>
       )}
+
+{/* This works, but the data in the fetched data is wrong */}
+<h2>My Bets</h2>
+{bets
+  ?.filter(bet => {
+    const currentDate = new Date();
+    const betDate = new Date(bet.commence_time);
+    return betDate.getDate() === currentDate.getDate() &&
+           betDate.getMonth() === currentDate.getMonth() &&
+           betDate.getFullYear() === currentDate.getFullYear();
+  })
+  .slice(-1)
+  .map((bet, index) => (
+    <div key={index}>
+      <h3>{bet.away_team} @ {bet.home_team}</h3>
+      <p>Date: {bet.commence_time}</p>
+      <p>Pick: {bet.pick}</p>
+      <p>Spread: {bet.spread}</p>
     </div>
+  ))
+}
+
+       {/* <h2>My Bets</h2>
+      {bets?.map((bet, index) => (
+          <div>
+            <h3>{bet.away_team} @ {bet.home_team}</h3>
+            <p>Date: {bet.commence_time}</p>
+            <p>Pick: {bet.pick}</p>
+            <p>Spread: {bet.spread}</p>
+          </div>
+      ))}  */}
+     
+      </div>
   )
 
 };
 export default Games;
+
+
